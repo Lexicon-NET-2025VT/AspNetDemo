fb3f38789f0bc9eddc48271256f58e5f740007d3
Pontus Wittenmark <pontus.wittenmark@gmail.com>
2025-05-12 12:09:55 +0200

    Add project files.

---
+++ AspNetDemo.Web/Program.cs
@@ -0,0 +1,15 @@
+namespace AspNetDemo.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/test", () => "Hello Test!");
+
+        app.Run();
+    }
+}
